4b7b06f3af4e7f7ffbd300c98ef65d6f3dfff54b
504 <504@DESKTOP-HH4VOLC>
2022-03-18 17:19:29 +0900

    ㅇㅇ

--- TeamPtoject1/src/Day01_설계/주차.java
+++ TeamPtoject1/src/Day01_설계/주차.java
@@ -8,10 +8,10 @@ public class 주차 {
 	// constructor
 	
 	// method
-	void 층수 (int floor) {
-		System.out.println("주차장이 설정되었습니다.");
-		for(int i = 0 ; i < 메인.)
-	}
+//	void 층수 (int floor) {
+//		System.out.println("주차장이 설정되었습니다.");
+//		
+//	}
 	void 입차 () {}
 	
 }
--- TeamPtoject1/src/Day01_설계/컨트롤러.java
+++ TeamPtoject1/src/Day01_설계/컨트롤러.java
@@ -1,5 +1,5 @@
 package Day01_설계;
 
 public class 컨트롤러 {
-
+ 
 }
