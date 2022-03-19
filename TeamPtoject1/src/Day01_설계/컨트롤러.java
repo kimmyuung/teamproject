0679c1c688b8ed52c283f36d0d91097b09633f5f
godoklife <96913067+godoklife@users.noreply.github.com>
2022-03-19 21:20:40 +0900

    파일로딩저장

--- TeamPtoject1/src/Day01_설계/컨트롤러.java
+++ TeamPtoject1/src/Day01_설계/컨트롤러.java
@@ -20,23 +20,17 @@ public class 컨트롤러 {
 			fileInputStream.read(임시바이트배열);	// 주차장매출.txt 파일 읽어들인 다음 바이트형 배열에 저장
 			String 파일내용 = new String(임시바이트배열);	// 바이트배열-> 문자열로 이동
 			
-			String[] 연매출 = 파일내용.split("\n");	
+			String[] 파일내용배열 = 파일내용.split("\n");	
 			
-			for(String tmp : 연매출) {
-				String[] 월매출 = tmp.split("/t");
-				for(String tmp2 : 월매출) {
-					// 파일로딩부 아직 제작중. 저장을 어캐할지 확정하고나서 완성 ㄱㄱ
-					// 햇갈리면 Day10_07 키오스크, 롯데리아 참고
-					
-//					매출 매출 = new 매출(field[0], field[1], field[2], field[3]);
-//					총매출저장리스트.add(월매출);	// 총매출저장리스트 리스트에 저장.
-				}
+			for(String tmp : 파일내용배열) {
+				String[] 임시배열 = tmp.split("/t");
+					매출 매출 = new 매출(Integer.parseInt(임시배열[0]), Integer.parseInt(임시배열[1]), 임시배열[2], 임시배열[3]);
+					총매출저장리스트.add(매출);	// 총매출저장리스트 리스트에 저장.
 			}
-			
 		} catch (Exception e) {System.out.println("매출파일로딩 메서드 예외 발생");}
 	}	// 매출파일로딩 메서드 END
 	
-	
+	// 하드에 저장된 txt파일 읽어들이는 메서드
 	public void 매출파일저장() {
 		try {
 			FileOutputStream fileOutputStream = new FileOutputStream("D:/java/주차장매출.txt");	// 1. 파일 출력 객체 생성
@@ -46,4 +40,5 @@ public class 컨트롤러 {
 			}
 		}catch(Exception e) {System.out.println("매출파일저장 메서드 예외 발생");}
 	}	// 매출파일저장 END
+	
 }	// class END
