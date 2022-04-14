815712d4bb3b7896850790268a4444303f541e7a
504 <504@DESKTOP-03H65CS>
2022-04-14 17:17:06 +0900

    HIHI

--- 자바FX_영화관/src/application/Main.java
+++ 자바FX_영화관/src/application/Main.java
@@ -14,6 +14,7 @@ public class Main extends Application {
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
+			
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
