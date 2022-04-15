4c2115321c5d0e8cbc1414d7c436b247139d6c36
504 <504@DESKTOP-HH4VOLC>
2022-04-15 19:40:55 +0900

    영화만

---
+++ 자바FX_영화관/src/application/Start.java
@@ -0,0 +1,32 @@
+package application;
+
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.image.Image;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+
+public class Start extends Application{
+@Override
+public void start(Stage stage) throws Exception {
+		// 5. 컨테이너 불러오기
+	Parent parent = 
+			FXMLLoader.load(getClass().getResource("/view/ahome.fxml"));
+	Scene scene = new Scene(parent);
+	stage.setScene(scene);
+	
+		
+	stage.setResizable(false); // 스테이지 크기 변경 불가
+	stage.setTitle("이젠영화"); // 스테이지 열기
+	stage.show();
+}
+public static void main(String[] args) {
+	launch(args);
+	
+}
+}
---
+++ 자바FX_영화관/src/controller/theaterlist/Tlist.java
@@ -0,0 +1,40 @@
+package controller.theaterlist;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+
+public class Tlist {
+ 
+	@FXML
+    private BorderPane borederpane;
+
+    @FXML
+    private AnchorPane anchorpane;
+
+    @FXML
+    private Button btnmlist;
+
+    @FXML
+    private Button btntlist;
+
+    @FXML
+    private Button btnslist;
+
+    @FXML
+    void mlist(ActionEvent event) {
+
+    }
+
+    @FXML
+    void slist(ActionEvent event) {
+
+    }
+
+    @FXML
+    void tlist(ActionEvent event) {
+
+    }	
+}
